6421bcc7a2b6cdbfc5156bd4aea9c8bbf90903ef
Alphons Joseph <93847055+Crazypersonalph@users.noreply.github.com>
2025-06-29 17:26:22 +0800

    login is done (but not auth and stuff, so don't get your hopes up)

--- src/api/auth/login/mod.rs
+++ src/api/auth/login/mod.rs
@@ -1,10 +1,23 @@
 use ::chrono::Utc;
 use sqlx::{postgres::{PgQueryResult, PgRow}, types::chrono, Executor, PgPool};
 
-use axum::{extract::State, handler::Handler, http::StatusCode, Error, Json, response::{IntoResponse, Response}};
+use axum::{extract::State, handler::Handler, http::StatusCode, Error, Json, response::{IntoResponse, Response}, body::Body };
 use uuid::Uuid;
 
 use serde::{Serialize, Deserialize};
+use serde_json::Value;
+
+use argon2::{
+    password_hash::{
+        rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, Salt, SaltString
+    },
+    Argon2
+};
+
+use jsonwebtoken::{encode, Header, EncodingKey, Algorithm};
+use std::time::{SystemTime, Duration, UNIX_EPOCH};
+
+use std::env;
 
 #[derive(Serialize, Deserialize, sqlx::Type)]
 #[sqlx(type_name = "provider")] // Use your actual Postgres enum name here
@@ -29,30 +42,88 @@ pub struct User {
     #[serde(rename = "pfpUrl")]
     pfp_url: Option<String>,
     #[serde(rename = "createdAt")]
-    created_at: Option<chrono::DateTime<Utc>>
+    created_at: Option<chrono::DateTime<Utc>>,
+    salt: Option<String>
 
 }
 
-pub async fn login(State(state): State<PgPool>) -> Json<Vec<User>> {
-    let result = sqlx::query_as!(
-        User,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Token {
+    sub: String,
+    exp: usize,
+    iat: usize,
+    username: String,
+    email: String
+}
+
+
+pub async fn login(State(state): State<PgPool>, Json(payload): Json<Value>) -> Response {
+    let email = payload["email"].as_str().unwrap();
+    let password = payload["password"].as_str().unwrap();
+    let users = sqlx::query!(
         r#"
-        SELECT 
+        SELECT
+            password,
+            salt,
             userid,
             email,
-            password,
-            provider::TEXT as "provider: Provider",
-            "providerId" as provider_id,
-            username,
-            "displayName" as display_name,
-            "pfpUrl" as pfp_url,
-            "createdAt" as created_at
+            username
         FROM users
-        "#
+        WHERE email = $1
+        "#,
+        email
     )
     .fetch_all(&state)
     .await
     .expect("Login malfunctioned");
+
+
+    if users.iter().len() > 0 {
+        let user = users.first().expect("Something went wrong. User doesn't exist??");
+
+        let argon2 = Argon2::default();
+
+        let parsed_hash = PasswordHash::new(&user.password).expect("Unable to parse stored password hash");
+
+        match argon2.verify_password(&password.as_bytes(), &parsed_hash) {
+            Ok(_) => {
+
+                let now = SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .expect("Time went backwards")
+                    .as_secs() as usize;
+        
+                let expiration = now + 60 * 60 * 24 * 7; // Token valid for 7 days
+        
+
+                let claims = Token {
+                    sub: user.userid.to_string(),
+                    exp: expiration,
+                    iat: now,
+                    username: user.username.to_string(),
+                    email: user.email.to_string()
+                };
+
+                let key = env::var("JWT_KEY").expect("No JWT signing secret found");
+
+                let token = jsonwebtoken::encode(&Header::default(), &claims, &EncodingKey::from_secret(&key.as_ref())).expect("JWT unable to be created");
+                return Response::builder()
+                    .status(StatusCode::OK)
+                    .body(Body::from(Json(token).to_string()))
+                    .unwrap()
+            },
+            Err(_) => {
+                return Response::builder()
+                    .status(StatusCode::UNAUTHORIZED)
+                    .body(Body::from(Json(String::from("Invalid email or password")).to_string()))
+                    .unwrap()
+            }
+        };
+    }
+
     
-    Json(result)
+    Response::builder()
+                    .status(StatusCode::UNAUTHORIZED)
+                    .body(Body::from(Json(String::from("Invalid email or password")).to_string()))
+                    .unwrap()
 }
