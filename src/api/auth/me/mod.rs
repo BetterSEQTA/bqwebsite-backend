6133f71d193231bbaf010042fce446f8a3f58d6d
Alphons Joseph <93847055+Crazypersonalph@users.noreply.github.com>
2025-08-11 20:54:45 +0800

    me returns your token

--- src/api/auth/me/mod.rs
+++ src/api/auth/me/mod.rs
@@ -1,3 +1,6 @@
-pub async fn me() -> String {
-    "me".to_string()
+use axum::{response::IntoResponse, Extension};
+use crate::types::Token;
+
+pub async fn me(Extension(token): Extension<Token>) -> impl IntoResponse {
+    format!("{:?}", token)
 }
